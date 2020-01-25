4fa7a083cfe3c896245e9422f08e4e48a4b23366
Nijesh KU <nijeshku1992@gmail.com>
2020-01-25 18:15:03 +0700

    Enable click

--- src/components/LayoutComponents/TopBar/index.js
+++ src/components/LayoutComponents/TopBar/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import ProjectManagement from './ProjectManagement'
+import { Link } from 'react-router-dom'
 import styles from './style.module.scss'
+
+import ProjectManagement from './ProjectManagement'
 
 class TopBar extends React.Component {
   render() {
     return (
       <div className={styles.topbar}>
         <div className="mr-4">
-          <ProjectManagement id="topBar.overallSpendView" />
+          <Link to="/dashboard/OverallSpendView">
+            <ProjectManagement id="topBar.overallSpendView" />
+          </Link>
         </div>
         <div className="mr-4">
-          <ProjectManagement id="topBar.functionView" />
+          <Link to="/dashboard/FunctionView">
+            <ProjectManagement id="topBar.functionView" />
+          </Link>
         </div>
         <div className="mr-4">
-          <ProjectManagement id="topBar.projectView" />
+          <Link to="/dashboard/ProgramView">
+            <ProjectManagement id="topBar.projectView" />
+          </Link>
         </div>
       </div>
     )
