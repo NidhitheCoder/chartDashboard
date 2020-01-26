f8d40585a12e98f30ad2d60192b211013a943110
Shekhar Sasikumar <shekhar.sasikumar@atkearney.com>
2020-01-26 10:19:32 +0800

    - Changed benefit values - Added click link from function view to program view

--- src/pages/dashboard/FunctionView/components/ProgramCard.js
+++ src/pages/dashboard/FunctionView/components/ProgramCard.js
@@ -1,5 +1,6 @@
 import cn from 'classnames'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import ChartistGraph from 'react-chartist'
 import ChartistTooltip from 'chartist-plugin-tooltips-updated'
 import styles from './style.module.scss'
@@ -20,10 +21,12 @@ class ProgramCard extends React.Component {
 
     return (
       <div className={cn('card card--withShadow', styles.paymentCard)}>
-        <div className={styles.titleContainer}>
-          <span className={styles.title}>{title}</span>
-          <div className={cn(styles.health, styles[healthStatus])}>&nbsp;</div>
-        </div>
+        <Link to="/dashboard/ProgramView">
+          <div className={styles.titleContainer}>
+            <span className={styles.title}>{title}</span>
+            <div className={cn(styles.health, styles[healthStatus])}>&nbsp;</div>
+          </div>
+        </Link>
         <div className={styles.chartContainer}>
           <ChartistGraph
             className={styles.chart}
